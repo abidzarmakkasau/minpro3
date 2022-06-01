69ec824b13ba060468fd0d4ef06545bf7aaf8a39
ihsanuddienullah <ihsanuddienullah@gmail.com>
2022-06-01 22:11:30 +0700

    add image, async fetch, loading

--- src/components/card/card-component.js
+++ src/components/card/card-component.js
@@ -4,7 +4,7 @@ import { Card, Button } from "react-bootstrap";
 const CardComponent = (props) => {
     return (
         <Card className="card-component">
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text>Rp {props.price}</Card.Text>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,33 +2,49 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { getAllProducts } from "../../services/api";
 import CardComponent from "../../components/card/card-component";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import ReactLoading from "react-loading";
 
 const Home = () => {
     const [dataProducts, setDataProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        getAllProducts()
+    const fetchGetAllProduct = async () => {
+        await setIsLoading(true);
+        await getAllProducts()
             .then((response) => setDataProducts(response.data))
             .catch((error) => {
                 console.log(error);
             });
+        await setIsLoading(true);
+    };
+
+    useEffect(() => {
+        fetchGetAllProduct();
     }, []);
 
     return (
         <div>
-            <Container className="mt-5">
-                <div className="d-flex flex-wrap">
-                    {dataProducts.map((product) => (
-                        <Link to="/detail-product" className="text-decoration-none">
-                            <CardComponent
-                                title={product.name}
-                                price={product.price}
-                            />
-                        </Link>
-                    ))}
-                </div>
-            </Container>
+            {isLoading ? (
+                <ReactLoading type="spinningBubbles" color="#0D6EFD" className="m-auto mt-5"/>
+            ) : (
+                <Container className="mt-5">
+                    <div className="d-flex flex-wrap">
+                        {dataProducts.map((product) => (
+                            <Link
+                                to="/detail-product"
+                                className="text-decoration-none"
+                            >
+                                <CardComponent
+                                    title={product.name}
+                                    price={product.price}
+                                    image={product.image}
+                                />
+                            </Link>
+                        ))}
+                    </div>
+                </Container>
+            )}
         </div>
     );
 };
